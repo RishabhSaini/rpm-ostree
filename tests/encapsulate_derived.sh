1fcddefa106f043f98def94a6506ef2815a84dd7
RishabhSaini <rsaini@redhat.com>
2023-02-01 14:59:47 -0500

    Adding --compare-with-build to cli

--- tests/encapsulate_derived.sh
+++ tests/encapsulate_derived.sh
@@ -3,10 +3,13 @@ set -xeuo pipefail
 # Pull the latest FCOS build, unpack its container image, and verify
 # that we can re-encapsulate it as chunked.
 
-ver=37.20230126.20.0
+ver=37.20230127.20.0
+packaging_ver=37.20230126.20.0
+compare_ver=37.20221117.20.0
 container=quay.io/fedora/fedora-coreos:${ver}
 
 tmpdir=/tmp/coreTest-${ver}
+p_tmpdir=/tmp/coreTest-${packaging_ver}
 mkdir -p ${tmpdir}
 cd ${tmpdir}
 ostree --repo=repo init
@@ -17,7 +20,6 @@ ostree container unencapsulate --write-ref=testref --repo=repo ostree-unverified
 
 # Re-pack it as a (chunked) container
 start=`date +%s.%N`
-/home/redhat/work/dev-updates-remoting/rpm-ostree/rpm-ostree compose container-encapsulate --repo=repo testref oci:${tmpdir}/test.oci
+/home/redhat/work/dev-updates-remoting/rpm-ostree/rpm-ostree compose container-encapsulate --compare-with-build=oci:/tmp/coreTest-${compare_ver}/test.oci --prior-build=oci:/tmp/coreTest-${packaging_ver}/test.oci --repo=repo testref oci:${tmpdir}/test.oci
 end=`date +%s.%N`
 runtime=$( echo "$end - $start" | bc -l )
->>>>>>> 8a85f660 (Adding --compare-with-build to cli)
